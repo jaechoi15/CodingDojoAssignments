c1be1a8be83549d39cf1db25b56e49953a9aadf6
JaeChoi <jchoi625@gmail.com>
2018-01-13 21:34:56 -0500

    updated car and added product assignment

--- Python/Car/car.py
+++ Python/Car/car.py
@@ -8,29 +8,21 @@ class Car(object):
         self.speed = speed
         self.fuel = fuel
         self.mileage = mileage
-        self.tax = 0.12
         
         if self.price > 10000:
             self.tax = 0.15
+        else:
+            self.tax = 0.12
+            
+        self.display_all()
 
     def display_all(self):
         print "Price: ${} Speed: {}mph; Fuel: {}; Mileage: {}mpg; Tax: {}".format(str(self.price), str(self.speed), str(self.fuel), str(self.mileage), str(self.tax))
         return self
 
 car1 = Car(2000, 35, "Full", 15)
-car1.display_all()
-
 car2 = Car(2000, 5, "Not Full", 105)
-car2.display_all()
-
 car3 = Car(2000, 15, "Kind of Full", 95)
-car3.display_all()
-
 car4 = Car(2000, 25, "Full", 25)
-car4.display_all()
-
 car5 = Car(2000, 45, "Empty", 25)
-car5.display_all()
-
 car6 = Car(20000000, 35, "Empty", 15)
-car6.display_all()
