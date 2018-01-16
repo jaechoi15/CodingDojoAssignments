84a60a14fb152f1a9771e8b817e63f2d06339104
JaeChoi <jchoi625@gmail.com>
2018-01-16 00:13:36 -0500

    added Django

---
+++ Python/Django/DjangoAppAssignment/main/apps/blogs/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render, HttpResponse, redirect
+
+def index(request):
+    response = "placeholder to later display all the list of blogs"
+    return HttpResponse(response)
---
+++ Python/Django/Virtualenv/djangoEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jaechoi8\desktop\codingdojoassignments\python\django\virtualenv\djangoenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
