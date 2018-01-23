c96768ba8f262fca14b0138a6e865bb2e4b71ff0
JaeChoi <jchoi625@gmail.com>
2018-01-23 00:11:12 -0500

    updated books_authors

--- Python/Django/DojoNinjas/main/apps/books_authors/models.py
+++ Python/Django/DojoNinjas/main/apps/books_authors/models.py
@@ -13,6 +13,7 @@ class Author(models.Model):
 	first_name = models.CharField(max_length=255)
 	last_name = models.CharField(max_length=255)
 	email = models.CharField(max_length=255)
-	book = models.ForeignKey(Book, related_name="authors")
+	books = models.ManyToManyField(Book, related_name="authors")
+	notes = models.TextField()
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
