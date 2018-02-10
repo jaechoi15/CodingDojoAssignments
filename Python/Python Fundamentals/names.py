65f388cc709df69d24adb185a8c0eaca8df1e919
JaeChoi <jchoi625@gmail.com>
2018-02-10 16:27:05 -0500

    added dojo_survey assignment

--- Python/Python Fundamentals/names.py
+++ Python/Python Fundamentals/names.py
@@ -1,20 +1,17 @@
 # Part I
 # Given the following list. Create a function.
-students = [
-     {'first_name':  'Michael', 'last_name' : 'Jordan'},
-     {'first_name' : 'John', 'last_name' : 'Rosales'},
-     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-     {'first_name' : 'KB', 'last_name' : 'Tonel'}
-]
+# students = [
+#      {'first_name':  'Michael', 'last_name' : 'Jordan'},
+#      {'first_name' : 'John', 'last_name' : 'Rosales'},
+#      {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+#      {'first_name' : 'KB', 'last_name' : 'Tonel'}
+# ]
 
-def student_names(lst):
-    for i in range(0,len(lst)):
-        print lst[i]["first_name"], lst[i]["last_name"]
+# def student_names(lst):
+#     for x in students:
+#         print x["first_name"], x["last_name"]
 
-student_names(students)
-
-# for student in students:
-#     print student["first_name"], student["last_name"]
+# student_names(students)
 
 # Part II
 # Given the following dictionary. Create a function.
@@ -31,16 +28,16 @@ users = {
   ]
  }
 
-def students_instructors(users):
+def students_instructors(mydictionary):
     for key in users:
         print key
         position = 0
 
-        for person in users[key]:
+        for user in users[key]:
             position += 1
-            first_name = person["first_name"].upper()
-            last_name = person["last_name"].upper()
-            name_length = len(person["first_name"]) + len(person["last_name"])
-            print position, "-", first_name, last_name, "-", name_length 
+            first_name = user["first_name"].upper()
+            last_name = user["last_name"].upper()
+            name_length = len(first_name) + len(last_name)
+            print position, first_name, last_name, "-", name_length
 
 students_instructors(users)
