b1e79d4f8d26a14ec0c453637495ccc495bdfffa
JaeChoi <jchoi625@gmail.com>
2018-02-10 20:56:14 -0500

    added counter assignment

---
+++ Python/Flask Fundamentals/counter/server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, session, redirect
+app = Flask(__name__)
+app.secret_key = "secretkey"
+
+@app.route('/')
+def index():
+    try:
+        session['counter'] += 1
+    except:
+        session['counter'] = 1
+    print session['counter']
+    return render_template("index.html")
+
+@app.route('/add', methods=['POST'])
+def add():
+    print "add 2"
+    session['counter'] += 1
+    return redirect('/')
+
+@app.route('/reset', methods=['POST'])
+def reset():
+    print "reset to 1"
+    session['counter'] = 0
+    return redirect('/')
+
+app.run(debug=True)
