8b1be18e4f40cbf41e9bf3a0b73eb1bce40d8bf6
JaeChoi <jchoi625@gmail.com>
2018-02-20 12:52:55 -0500

    added sakila assignment

--- Python/MySQL/MySQLQueries/mysql_countries.sql
+++ Python/MySQL/MySQLQueries/mysql_countries.sql
@@ -1,3 +1,5 @@
+USE world;
+
 /*1. What query would you run to get all the countries that speak Slovene? 
 Your query should return the name of the country, language and language percentage. 
 Your query should arrange the result by language percentage in descending order. (1)*/
